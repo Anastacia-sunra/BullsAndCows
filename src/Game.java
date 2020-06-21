5c23337c718edf138fc76fbd53e2a55102017b3f
Anastacia-sunra <sunraJVR@gmail.com>
2020-06-21 18:45:31 +0300

    Change Main to Game

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Game {
 
     private static Scanner scanner = new Scanner(System.in);
 
